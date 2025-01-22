5c0811b798be733ca452592559545e84f0d942e0
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-01-22 09:21:55 -0800

    Create BellConsoleSystem.cs

---
+++ Content.Client/Bell/BellConsoleSystem.cs
@@ -0,0 +1,6 @@
+// Taken from https://github.com/emberfall-14/emberfall/pull/4/files with permission
+using Content.Shared.TP14.Bell.Systems;
+
+namespace Content.Client.TP14.Bell.Systems;
+
+public sealed class SpaceElevatorConsoleSystem : SharedSpaceElevatorConsoleSystem;
