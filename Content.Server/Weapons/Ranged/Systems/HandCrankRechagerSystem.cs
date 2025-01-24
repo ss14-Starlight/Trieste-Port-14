27f220b954099d18e9c160e467bc3695cd9915f1
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-01-24 10:45:10 -0800

    Create HandCrankRechagerSystem.cs

---
+++ Content.Server/Weapons/Ranged/Systems/HandCrankRechagerSystem.cs
@@ -0,0 +1,58 @@
+using Content.Server.Power.Components;
+using Content.Server.Power.EntitySystems;
+using Content.Server.Power.Events;
+using Content.Shared.Weapons.Ranged.Components;
+using Content.Shared.Verbs;
+using Content.Shared.DoAfter;
+
+
+namespace Content.Server.Weapons.Ranged.Systems;
+{
+    public sealed class HandCrankRechargerSystem : EntitySystem
+    {
+        [Dependency] private readonly BatterySystem _battery = default!;
+        [Dependency] private readonly SharedDoAfterSystem _doAfterSystem = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<HandCrankRechargerComponent , GetVerbsEvent<ActivationVerb>>(AddCrankVerb);
+            SubscribeLocalEvent<HandCrankRechargerComponent, GunCrankAfterEvent>(OnDoAfter);
+        }
+
+          private void AddToggleAnalysisVerb(EntityUid uid, HandCrankRechargerComponent component, GetVerbsEvent<ActivationVerb> args)
+        {
+            if (!args.CanAccess || !args.CanInteract)
+            return;
+
+          ActivationVerb verb = new()
+          {
+              Text = Loc.GetString("toggle-hand-crank-text"),
+              Icon = new SpriteSpecifier.Texture(new ("/Textures/Interface/VerbIcons/light.svg.192dpi.png")),
+              Act = () => StartDoAfter(uid, component)
+              Priority = -1 // For things like PDA's, Open-UI, etc.
+           };
+
+            args.Verbs.Add(verb);
+         }
+
+
+           private void StartDoAfter(EntityUid uid, HandCrankRechargerComponent component)
+           {
+            _doAfterSystem.TryStartDoAfter((new DoAfterArgs(EntityManager, uid, component.TimeToCrank, new GunCrankAfterEvent()));
+           }
+
+
+       private void OnDoAfter(EntityUid uid, HandCrankRechargerComponent gun, DoorPryDoAfterEvent args)
+      {
+        if (!TryComp<BatteryComponent>(uid, out var gunBattery))
+            continue;
+
+        _battery.SetCharge(gunBattery.Owner, gunBattery.CurrentCharge + gun.AmountRecharged, gunBattery);
+      }
+                                           
+    [Serializable, NetSerializable]
+    public sealed partial class GunCrankAfterEvent : SimpleDoAfterEvent;
+    {
+    }
+}
