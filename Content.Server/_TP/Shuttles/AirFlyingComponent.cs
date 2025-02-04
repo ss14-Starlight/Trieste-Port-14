04332cedb562c897f00e355248995c98a8260f34
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-02-04 11:10:49 -0800

    Update AirFlyingComponent.cs

--- Content.Server/_TP/Shuttles/AirFlyingComponent.cs
+++ Content.Server/_TP/Shuttles/AirFlyingComponent.cs
@@ -1,7 +1,7 @@
 using Content.Shared.EntityList;
 using Robust.Shared.Prototypes;
 
-namespace Content.Server._TP.Shuttles;
+namespace Content.Server._TP.Shuttles_components;
 
 [RegisterComponent]
 public sealed partial class AirFlyingComponent : Component
