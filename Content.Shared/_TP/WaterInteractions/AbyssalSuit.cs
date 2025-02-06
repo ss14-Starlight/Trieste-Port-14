b5884776e983960ce6f698640f7981c1991c5632
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-02-06 11:28:34 -0800

    Create AbyssalSuit.cs

---
+++ Content.Shared/_TP/WaterInteractions/AbyssalSuit.cs
@@ -0,0 +1,11 @@
+namespace Content.Shared.TP.Abyss.Components
+
+// Summary//
+// This component marks a suit as a protection against abyssal pressures, protecting you from crush depth.
+// Summary//
+
+[RegisterComponent]
+public sealed partial class AbyssalSuit : Component
+{
+
+}
