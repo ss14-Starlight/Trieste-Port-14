cb0569858523f7af007a3ce6df1dde0f8a0446c9
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-02-26 10:14:29 -0800

    Create RainCrushableComponent.cs

---
+++ Content.Shared/_TP/RWEvent/RainCrushableComponent.cs
@@ -0,0 +1,12 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Weather;
+
+/// <summary>
+/// This entity will mark objects are crushable by The Rain.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class RainCrushableComponent : Component
+{
+
+}
