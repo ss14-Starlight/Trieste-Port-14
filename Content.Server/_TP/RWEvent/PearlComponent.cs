6c43b49d9d1e3c2b0aa75f74ceb733183575890a
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-02-26 10:56:16 -0800

    Create PearlComponent.cs

---
+++ Content.Server/_TP/RWEvent/PearlComponent.cs
@@ -0,0 +1,15 @@
+﻿namespace Content.Server.TP.Event.Components;
+
+using Content.Server.Xenoarchaeology.XenoArtifacts;
+using Content.Shared.Construction.Prototypes;
+using Robust.Shared.Audio;
+using Robust.Shared.Serialization.TypeSerializers.Implementations;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Prototypes;
+
+[RegisterComponent]
+public sealed partial class PearlComponent : Component
+{
+    [DataField("pearlMessage")]
+    private string PearlMessage = "Unknown";
+}
