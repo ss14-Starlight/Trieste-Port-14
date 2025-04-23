eaa1a1db08451413ce639b07f7a922ef9eff6653
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-04-23 10:06:17 -0700

    Create RadiantTemperatureComponent.cs

---
+++ Content.Server/Temperature/Components/RadiantTemperatureComponent.cs
@@ -0,0 +1,25 @@
+// Credit to git pull https://github.com/space-wizards/space-station-14/pull/32087
+
+using Content.Shared.Atmos;
+
+namespace Content.Server.Temperature.Components;
+
+/// <summary>
+/// The entity will cause the surrounding air temperature to change passively without
+/// any need for power or anything else.
+/// </summary>
+[RegisterComponent]
+public sealed partial class RadiantTemperatureComponent : Component
+{
+    /// <summary>
+    /// The temperature that the entity will try to reach
+    /// </summary>
+    [DataField]
+    public float GoalTemperature = Atmospherics.T800C;
+
+    /// <summary>
+    /// How much energy (in joules) to add to or remove from the surrounding air per second
+    /// </summary>
+    [DataField]
+    public float EnergyChangedPerSecond = 10f;
+}
