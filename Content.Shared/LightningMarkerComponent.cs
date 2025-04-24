c6c2008ec107cfe09c100801aff23c33e28d2711
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-04-24 10:54:09 -0700

    Update LightningMarkerComponent.cs

--- Content.Shared/LightningMarkerComponent.cs
+++ Content.Shared/LightningMarkerComponent.cs
@@ -6,7 +6,16 @@ namespace Content.Shared.Gravity
     [RegisterComponent]
     public sealed partial class LightningMarkerComponent : Component
     {
+        // The lightning prototype this marker should spawn
         [DataField("lightningPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
         public string LightningPrototype = "AdminInstantEffectThunder";
+
+        // The range in which lightning will occur
+        [DataField("lightingRange")]
+        public float LightningRange = 70f;
+
+        // How often (in seconds) the lightning will occur
+        [DataField("lightingFrequency")]
+        public float LightningFrequency = 8f;
     }
 }
