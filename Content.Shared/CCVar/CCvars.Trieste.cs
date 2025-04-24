c04c5fbf1de47be095be9c738899f6585d303ac3
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-04-24 10:50:42 -0700

    Update CCvars.Trieste.cs

--- Content.Shared/CCVar/CCvars.Trieste.cs
+++ Content.Shared/CCVar/CCvars.Trieste.cs
@@ -7,6 +7,9 @@ public sealed partial class CCVars
     public static readonly CVarDef<float> ThunderFrequency =
         CVarDef.Create("trieste.ThunderFrequency", 10f, CVar.SERVERONLY);
 
+     public static readonly CVarDef<bool> CustomThunder =
+        CVarDef.Create("trieste.CustomThunder", false, CVar.SERVERONLY);
+
     public static readonly CVarDef<float> ThunderRange =
         CVarDef.Create("trieste.ThunderRange", 50f, CVar.SERVERONLY);
 }
