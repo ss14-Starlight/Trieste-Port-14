8de57648c6bda52ee833b95ce6519cec53f2daee
Pixeltheaertist <126490507+Pixeltheaertist@users.noreply.github.com>
2025-04-24 10:46:01 -0700

    Create FlashStormRuleComponent.cs

---
+++ Content.Server/StationEvents/Components/FlashStormRuleComponent.cs
@@ -0,0 +1,15 @@
+using Content.Server.StationEvents.Events;
+using Robust.Shared.Prototypes;
+using Content.Shared.Weather;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.List;
+
+
+namespace Content.Server.StationEvents.Components;
+
+[RegisterComponent, Access(typeof(FlashStormRule))]
+public sealed partial class FlashStormRuleComponent : Component
+{
+   [DataField("stormWeather", required: true, customTypeSerializer:typeof(PrototypeIdSerializer<WeatherPrototype>))]
+   public string StormWeather = string.Empty;
+}
